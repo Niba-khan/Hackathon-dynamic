af7fe724aee993690a0d084dd971c1af72826f32
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2025-01-22 01:26:03 +0500

    Add files via upload

---
+++ src/app/sanity/lib/client.ts
@@ -0,0 +1,11 @@
+import { createClient } from 'next-sanity'
+
+import { apiVersion, dataset, projectId } from '../env'
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+  token:"skaTQl0tBFpo4g2TLPRE6BzP05kJWxmDZA0pqZVuRZ6M77ArGkIVcqY63MHNXHjDOYwVIozif6KyiVwrTEKvsy18a4vsqfwsvem2Y84ho0UEMMe2rbuLhqx6ih3At0yuVJKgjHeoep9NHp5BC5hRk11Ya9sCzrjPVFTEugeau07WmTd6NuFi" // Set to false if statically generating pages, using ISR or tag-based revalidation
+})

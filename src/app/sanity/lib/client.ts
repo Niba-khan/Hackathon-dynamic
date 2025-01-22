23bb70026199cff9b4d9f150b15239b08a84291e
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2025-01-22 07:19:14 +0500

    Update client.ts

--- src/app/sanity/lib/client.ts
+++ src/app/sanity/lib/client.ts
@@ -1,7 +1,5 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
-
 export const client = createClient({
   projectId:"h6zorl2h",
   dataset: "production",
